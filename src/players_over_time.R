21b577d37026c800e17412d4e22db2ac99d61b02
allswellthatsmaxwell <allswellthatsmaxwell9@gmail.com>
2026-09-10 23:06:22 -0500

    neater person-of-interest

--- src/players_over_time.R
+++ src/players_over_time.R
@@ -7,6 +7,7 @@ library(stringr)
 library(magrittr)
 library(lubridate)
 library(assertthat)
+library(glue)
 
 assertSameWeeksForEveryone <- function(dat) {
   unique_weekcounts <- dat %>%
@@ -19,14 +20,24 @@ assertSameWeeksForEveryone <- function(dat) {
 a different number of weeks listed than others.")
 }
 
-getDataPaths <- function(data_dir, filter_str) {
+getDataPaths <- function(data_dir, filter_seasons) {
+  assert_that(all(filter_seasons %in% VALID_SEASONS), 
+              msg = glue("invalid season found in filter_seasons: '{filter_seasons}'"))
   paths <- list.files(data_dir) %>%
-    .[str_detect(., filter_str)] %>%
+    .[str_detect(., filter_seasons)] %>%
     sapply(. %>% paste0(data_dir, .))
+  assert_that(length(paths) > 0, 
+              msg = glue("getDataPaths: didn't find any seasons matching ", 
+                         "filter_seasons '{filter_seasons}'"))
+  paths
 }
 
-getPointsOverSeasonDat <- function(paths) {
-  readr::read_csv(paths) %>%
+getPointsOverSeasonFrame <- function(path) {
+  season <- str_extract(path, "(winter|spring|summer|fall)-\\d{4}")
+  assert_that(!is.na(season),
+              msg = glue("could not extract season name from path {path}"))
+  assert_that(length(path) == 1)
+  readr::read_csv(path) %>%
     dplyr::select(-`Total Points`) %>%
     tidyr::pivot_longer(!Player, names_to = "date", values_to = "points") %>%
     dplyr::rename_with(tolower) %>%
@@ -34,18 +45,38 @@ getPointsOverSeasonDat <- function(paths) {
                   date = lubridate::mdy(date)) %>%
     dplyr::arrange(player, date) %>%
     dplyr::group_by(player) %>%
-    dplyr::mutate(points_so_far = cumsum(points))
+    dplyr::mutate(points_so_far = cumsum(points),
+                  season = season)
 }
+
+# getPOIFrame <- function(players_of_interest) {
+#   lapply(names(players_of_interest),
+#          function(season) {
+#            players <- players_of_interest[[season]]
+#            lapply(players, function(player) data.frame(season, player)) %>%
+#              dplyr::bind_rows()
+#          }) %>%
+#     dplyr::bind_rows()
+# }
+
+
+DATA_DIR <- "data/"
+PLAYERS_OF_INTEREST_SEASONAL <-
+    list("summer-2026" = c("Maxwell Peterson", "Paul Gonzalez"),
+         "spring-2026" = c("Maxwell Peterson", "Jeremiah Stene"),
+         "winter-2026" = c("Maxwell Peterson", "Anna Kinkead"))
+
+VALID_SEASONS <- names(PLAYERS_OF_INTEREST_SEASONAL)
+
 
 setwd('fpn-analysis/')
 
-DATA_DIR <- "data/"
-paths <- getDataPaths(DATA_DIR, "summer")
-dat <- getPointsOverSeasonDat(paths)
+
+paths <- getDataPaths(DATA_DIR, "summer-2026")
+dat <- getPointsOverSeasonFrame(paths) %>%
+  mutate(of_interest = player %in% PLAYERS_OF_INTEREST_SEASONAL[season][[1]])
 assertSameWeeksForEveryone(dat)
 
-players_of_interest <- c("Maxwell Peterson", "Paul Gonzalez")
-dat %<>% mutate(of_interest = any(player == players_of_interest))
 
 dat %>%
   ggplot(aes(x = date, y = points_so_far, 
